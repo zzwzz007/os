c4feeff26779842dc027f1c829ee58f36a7af575
zzwzz07 <578835214@qq.com>
2020-11-15 05:35:07 -0800

    update

--- exp2/exp2.1.cpp
+++ exp2/exp2.1.cpp
@@ -14,7 +14,6 @@ union semun
     int val;               /* value for SETVAL */
     struct semid_ds *buf;  /* buffer for IPC_STAT, IPC_SET */
     unsigned short *array; /* array for GETALL, SETALL */
-                           /* Linux specific part: */
     struct seminfo *__buf; /* buffer for IPC_INFO */
 };
 
